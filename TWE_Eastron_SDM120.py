1f84842ea094fef47fd99c913ed26efb1fa214a3
Matthias Urlichs <matthias@urlichs.de>
2022-07-02 09:41:13 +0200

    add SDM120

---
+++ TWE_Eastron_SDM120.py
@@ -0,0 +1,89 @@
+# VenusOS module for support of Eastron SDM120-Modbus v1
+# might work also with other Eastron devices > Product code on 0x001c (type u16b) to be added into models overview
+# 
+
+import logging
+import TWE_Eastron_device as device
+import probe
+from register import *
+
+log = logging.getLogger()
+
+class Reg_f32b(Reg_num):
+    def __init__(self, base, *args, **kwargs):
+        super(Reg_f32b, self).__init__(base, 2, *args, **kwargs)
+        self.coding = ('>f', '>2H')
+        self.scale = float(self.scale)
+		
+nr_phases = [ 0, 1, 3, 3 ]
+
+phase_configs = [
+    'undefined',
+    '1P',
+    '3P.1',
+    '3P.n',
+]
+
+class Eastron_SDM120(device.EnergyMeter):
+    productid = 0xB023 # id assigned by Victron Support
+    productname = 'Eastron SDM120-Modbus'
+    min_timeout = 0.5
+    hardwareversion = '1'
+    firmwareversion = '0'
+    serial = '0'
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+        self.info_regs = [
+            Reg_u16( 0xfc02, '/HardwareVersion'),
+            Reg_u16( 0xfc03, '/FirmwareVersion'),
+            # Reg_f32b( 0x000a, '/PhaseConfig', text=phase_configs, write=(0, 3)),
+            Reg_u32b(0x0014, '/Serial'),
+        ]
+
+    def phase_regs(self, n):
+        s = 0x0002 * (n - 1)
+        return [
+            Reg_f32b(0x0000 + s, '/Ac/L%d/Voltage' % n,        1, '%.1f V'),
+            Reg_f32b(0x0006 + s, '/Ac/L%d/Current' % n,        1, '%.1f A'),
+            Reg_f32b(0x000c + s, '/Ac/L%d/Power' % n,          1, '%.1f W'),
+            Reg_f32b(0x0048 + s, '/Ac/L%d/Energy/Forward' % n, 1, '%.1f kWh'),
+            Reg_f32b(0x004a + s, '/Ac/L%d/Energy/Reverse' % n, 1, '%.1f kWh'),
+        ]
+
+    def device_init(self):
+
+        self.read_info()
+
+        phases = 1
+
+        regs = [
+            Reg_f32b(0x000c, '/Ac/Power',          1, '%.1f W'),
+            Reg_f32b(0x0006, '/Ac/Current',        1, '%.1f A'),
+            Reg_f32b(0x0046, '/Ac/Frequency',      1, '%.1f Hz'),
+            Reg_f32b(0x0048, '/Ac/Energy/Forward', 1, '%.1f kWh'),
+            Reg_f32b(0x004a, '/Ac/Energy/Reverse', 1, '%.1f kWh'),
+        ]
+
+        for n in range(1, phases + 1):
+            regs += self.phase_regs(n)
+
+        self.data_regs = regs
+
+    def get_ident(self):
+        return 'cg_%s' % self.info['/Serial']
+
+
+
+# identifier to be checked, if register identical on all SDM120 (only first 16 bytes in u16b of 32 bit register 0xfc02)
+models = {
+    16512: {
+        'model':    'SDM120Modbusv1',
+        'handler':  Eastron_SDM630v1,
+    },
+}
+
+probe.add_handler(probe.ModelRegister(0x001c, models,
+                                      methods=['tcp'],
+                                      units=[1]))
